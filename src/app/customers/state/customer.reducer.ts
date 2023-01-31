5de6e11d2b7195ad6abfc8d91d8b0a5d45aca92e
Anderson Guedes <anderson.guedes@outlook.com.br>
2023-01-31 09:39:55 -0300

    Add Reducer e Action de Load

--- src/app/customers/state/customer.reducer.ts
+++ src/app/customers/state/customer.reducer.ts
@@ -1,24 +1,53 @@
-const initialState = {
-  customers: [
-    {
-      id: 1,
-      name: 'Hambúrguer',
-      address: 'Rua I',
-      phone: '1935353535',
-      membeship: 'Platinum',
-    },
-  ],
+import * as customerActions from './customer.actions';
+import { Customer } from '../customer.model';
+import * as fromRoot from '../../state/app-state';
+
+export interface CustomerState {
+  customers: Customer[];
+  loading: boolean;
+  loaded: boolean;
+  error: string;
+}
+
+export interface AppState extends fromRoot.AppState {
+  customers: CustomerState;
+}
+
+export const initialState: CustomerState = {
+  customers: [],
   loading: false,
-  loaded: true,
+  loaded: false,
+  error: '',
 };
 
-export function customerReducer(state = initialState, action: any) {
+export function customerReducer(
+  state = initialState,
+  action: customerActions.Action
+): CustomerState {
   switch (action.type) {
-    case 'LOAD_CUSTOMERS': {
+    case customerActions.CustomerActionTypes.LOAD_CUSTOMERS: {
       return {
         ...state,
         loading: true,
+      };
+    }
+
+    case customerActions.CustomerActionTypes.LOAD_CUSTOMERS_SUCCESS: {
+      return {
+        ...state,
+        loading: false,
         loaded: false,
+        customers: action.payload,
+      };
+    }
+
+    case customerActions.CustomerActionTypes.LOAD_CUSTOMERS_FAIL: {
+      return {
+        ...state,
+        customers: [],
+        loading: false,
+        loaded: false,
+        error: action.payload,
       };
     }
 
