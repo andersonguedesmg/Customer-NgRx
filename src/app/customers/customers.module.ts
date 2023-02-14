e162960487c07232c39766b629bbfd5419cfb85a
Anderson Guedes <anderson.guedes@outlook.com.br>
2023-02-14 15:16:55 -0300

    Add CRUD de Cliente

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -5,6 +5,7 @@ import { CustomerAddComponent } from './customer-add/customer-add.component';
 import { CustomerEditComponent } from './customer-edit/customer-edit.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { customerReducer } from './state/customer.reducer';
 import { EffectsModule, Actions } from '@ngrx/effects';
@@ -21,6 +22,8 @@ const customerRoutes: Routes = [{ path: '', component: CustomerComponent }];
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
     RouterModule.forChild(customerRoutes),
     EffectsModule.forFeature([CustomerEffect]),
     StoreModule.forFeature('customers', customerReducer),
