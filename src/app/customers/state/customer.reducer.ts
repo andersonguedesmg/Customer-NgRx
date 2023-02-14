69d81c27360e045449fe4b12543977be82516344
Anderson Guedes <anderson.guedes@outlook.com.br>
2023-02-14 10:01:03 -0300

    Add Entity

--- src/app/customers/state/customer.reducer.ts
+++ src/app/customers/state/customer.reducer.ts
@@ -2,9 +2,10 @@ import * as customerActions from './customer.actions';
 import { Customer } from '../customer.model';
 import * as fromRoot from '../../state/app-state';
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
 
-export interface CustomerState {
-  customers: Customer[];
+export interface CustomerState extends EntityState<Customer> {
+  selectedCustomerId: number | null;
   loading: boolean;
   loaded: boolean;
   error: string;
@@ -14,12 +15,19 @@ export interface AppState extends fromRoot.AppState {
   customers: CustomerState;
 }
 
-export const initialState: CustomerState = {
-  customers: [],
+export const customerAdapter: EntityAdapter<Customer> =
+  createEntityAdapter<Customer>();
+
+export const defaultCustomer: CustomerState = {
+  ids: [],
+  entities: {},
+  selectedCustomerId: null,
   loading: false,
   loaded: false,
   error: '',
 };
+
+export const initialState = customerAdapter.getInitialState(defaultCustomer);
 
 export function customerReducer(
   state = initialState,
@@ -34,18 +42,17 @@ export function customerReducer(
     }
 
     case customerActions.CustomerActionTypes.LOAD_CUSTOMERS_SUCCESS: {
-      return {
+      return customerAdapter.setAll(action.payload, {
         ...state,
         loading: false,
-        loaded: false,
-        customers: action.payload,
-      };
+        loaded: true,
+      });
     }
 
     case customerActions.CustomerActionTypes.LOAD_CUSTOMERS_FAIL: {
       return {
         ...state,
-        customers: [],
+        entities: {},
         loading: false,
         loaded: false,
         error: action.payload,
@@ -63,7 +70,7 @@ const getCustomerFeatureState =
 
 export const getCustomers = createSelector(
   getCustomerFeatureState,
-  (state: CustomerState) => state.customers
+  customerAdapter.getSelectors().selectAll
 );
 
 export const getCustomersLoading = createSelector(
